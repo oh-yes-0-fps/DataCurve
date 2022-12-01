ef6ecc7858f25d379f840696f5374d6e8b753e7e
oh-yes-0-fps <foryt.bowan@gmail.com>
2022-12-01 01:17:14 -0500

    Double checked formulas

--- Main.py
+++ Main.py
@@ -5,8 +5,8 @@ from typing import Union, Any
 
 ONETHIRD: float = 1/3
 
-TANG_TENSION = 0
-TANG_MULTIPLIER = 1
+tang_tension = 0
+TANG_MULTIPLIER = 2
 
 
 class interpMode(Enum):
@@ -15,6 +15,7 @@ class interpMode(Enum):
     CUBIC = 2
     CONSTANT = 3
     LAGRANGE = 4
+    QUADRATIC = 5
 
     @staticmethod
     def fromStr(string: str):
@@ -43,6 +44,12 @@ class tangent:
         self.leave = _inFloat
         return self
 
+    def autoCalc(self, PrevTime: float, PrevPoint: float, CurTime: float, CurPoint: float, NextTime: float, NextPoint: float, Tension: float):
+        tang = curveMath.AutoCalcTangent(
+            PrevTime, PrevPoint, CurTime, CurPoint, NextTime, NextPoint, Tension)
+        self.arrive = tang
+        self.leave = tang
+
     def __repr__(self) -> str:
         return f"(a:{self.arrive}, l:{self.leave})"
 
@@ -57,14 +64,14 @@ class curveKey:
     def getTime(key) -> float:
         return key.time
 
-    def __init__(self, _time: float, _value: float, _mode: interpMode, _tangents: Union[tangent, tuple] = tangent()) -> None:
+    def __init__(self, _time: float, _value: float, _mode: interpMode, _tangents: Union[tangent, tuple] = (0, 0)) -> None:
         self.time = _time
         self.value = _value
         self.mode = _mode
         if isinstance(_tangents, tuple):
-            self.tangents: tangent = tangent().fromTuple(_tangents)
+            self.tangent: tangent = tangent().fromTuple(_tangents)
         else:
-            self.tangents = _tangents
+            self.tangent = _tangents
 
     def setMode(self, _mode: Union[str, interpMode]):
         if isinstance(_mode, str):
@@ -73,7 +80,7 @@ class curveKey:
             self.mode = _mode
 
     def __repr__(self) -> str:
-        return f"({self.time}, {self.value}, {self.mode.name}, {self.tangents})"
+        return f"({self.time}, {self.value}, {self.mode.name}, {self.tangent})"
 
 
 class curveMath:
@@ -81,7 +88,8 @@ class curveMath:
     def AutoCalcTangent(PrevTime: float, PrevPoint: float, CurTime: float, CurPoint: float, NextTime: float, NextPoint: float, Tension: float) -> float:
         outTan: float = (1 - Tension) * \
             ((CurPoint-PrevPoint) + (NextPoint-CurPoint))
-        PrevToNextTimeDiff = max(0.001, NextTime - PrevTime)
+        PrevToNextTimeDiff = max(0.1, NextTime - PrevTime)
+        print(f'{outTan} / {PrevToNextTimeDiff} = {outTan/PrevToNextTimeDiff}')
         return (outTan/PrevToNextTimeDiff) * TANG_MULTIPLIER
 
     @staticmethod
@@ -89,12 +97,29 @@ class curveMath:
         return A + (B-A)*T
 
     @staticmethod
-    def __QuadInterp(A: float, B: float, C: float, T: float) -> float:
-        return (A*(T**2)) + (B*T) + C
+    def __QuadInterp(Value1: float, Value2: float, Tangent1: float, Tangent2: float, T: float) -> float:
+        # A = P0, B = P1, C = P2, T = 0-1
+        # figure out C using the tangents
+        C = curveMath.__Lerp(Value2, Value1, T) - \
+            curveMath.__Lerp(Tangent2, Tangent1, T)
+
+        return curveMath.__Lerp(curveMath.__Lerp(Value1, Value2, T), C, T)
 
     @staticmethod
-    def __CubicInterp(A: float, B: float, C: float, D: float, T: float) -> float:
-        return (A*(T**3)) + (B*(T**2)) + (C*T) + D
+    def __CubicInterp(Value1: float, Value2: float, Tangent1: float, Tangent2: float, T: float) -> float:
+        # cubic bezier interpolation
+
+        # calculate the two control points
+        C1 = Value1 + Tangent1 * ONETHIRD
+        C2 = Value2 - Tangent2 * ONETHIRD
+
+        # calculate the two intermediate points
+        I1 = curveMath.__Lerp(Value1, C1, T)
+        I2 = curveMath.__Lerp(C1, C2, T)
+        I3 = curveMath.__Lerp(C2, Value2, T)
+
+        # calculate the final point
+        return curveMath.__Lerp(I1, I2, T) + curveMath.__Lerp(I2, I3, T)
 
     @staticmethod
     def lagrangeInterp(_keyList: list, _time: float) -> float:
@@ -122,19 +147,19 @@ class curveMath:
     def quadInterp(_Key1: curveKey, _Key2: curveKey, timeIn: float) -> float:
         diff = _Key2.time - _Key1.time
         timeOffset = (timeIn - _Key1.time) / diff
-        return curveMath.__QuadInterp(_Key1.value, _Key1.tangents.leave, _Key2.tangents.arrive, timeOffset)
+        return curveMath.__QuadInterp(_Key1.value, _Key2.value, _Key1.tangent.leave, _Key2.tangent.arrive, timeOffset)
 
     @staticmethod
     def cubicInterp(_Key1: curveKey, _Key2: curveKey, timeIn: float) -> float:
         diff = _Key2.time - _Key1.time
         timeOffset = (timeIn - _Key1.time) / diff
 
-        point0 = _Key1.value
-        point1 = _Key1.value + (_Key1.tangents.leave * diff * ONETHIRD)
-        point2 = _Key2.value - (_Key2.tangents.arrive * diff * ONETHIRD)
-        point3 = _Key2.value
+        # return curveMath.__CubicInterp(_Key1.value, _Key2.value, _Key1.tangents.leave, _Key2.tangents.arrive, timeOffset)
 
-        # return curveMath.__CubicInterp(point0, point1, point2, point3, timeOffset)
+        point0 = _Key1.value
+        point1 = _Key1.value + (_Key1.tangent.leave * diff * ONETHIRD)
+        point2 = _Key2.value - (_Key2.tangent.arrive * diff * ONETHIRD)
+        point3 = _Key2.value
 
         p01 = curveMath.__Lerp(point0, point1, timeOffset)
         p12 = curveMath.__Lerp(point1, point2, timeOffset)
@@ -147,7 +172,6 @@ class curveMath:
 
 class dataCurve:
     def __init__(self, *args: curveKey):
-        self.hasBeenFinalized: bool = False
         self.keys: list[curveKey] = []
         for i in args:
             if isinstance(i, curveKey):
@@ -155,8 +179,6 @@ class dataCurve:
         self.keys.sort(key=curveKey.getTime)
 
     def addKeys(self, *args: curveKey):
-        if self.hasBeenFinalized:
-            return
         for i in args:
             if isinstance(i, curveKey):
                 self.keys.append(i)
@@ -165,29 +187,20 @@ class dataCurve:
     def addKeysList(self, _inList: list[curveKey]):
         return self.addKeys(*_inList)
 
-    def finalize(self):
-        self.hasBeenFinalized = True
-        # self.keys[0].setMode(interpMode.LINEAR)
-        # self.keys[-1].setMode(interpMode.LINEAR)
+    def finalize(self, tension = 0):
         for i in range(len(self.keys)):
             _Ky = self.keys[i]
             if _Ky.mode == interpMode.CUBIC:
-                if i == 0:
-                    _Ky.tangents.leave = 0
-                if i == len(self.keys)-1:
-                    _Ky.tangents.arrive = 0
-                try:
-                    p_Ky = self.keys[i-1]
-                    n_Ky = self.keys[i+1]
-                    tang = curveMath.AutoCalcTangent(
-                        p_Ky.time, p_Ky.value, _Ky.time, _Ky.value, n_Ky.time, n_Ky.value, TANG_TENSION)
-                    _Ky.tangents.fromFloat(tang)
-                except:
-                    pass
+                if i == len(self.keys)-1 or i == 0:
+                    # _Ky.tangent.leave = 0
+                    # _Ky.tangent.arrive = 0
+                    continue
+                p_Ky = self.keys[i-1]
+                n_Ky = self.keys[i+1]
+                _Ky.tangent.autoCalc(
+                    p_Ky.time, p_Ky.value, _Ky.time, _Ky.value, n_Ky.time, n_Ky.value, tension)
 
     def eval(self, timeIn: float) -> float:
-        if not self.hasBeenFinalized:
-            return 0
         for i in range(len(self.keys)):
             _Ky = self.keys[i]
             if _Ky.time > timeIn:
@@ -198,6 +211,8 @@ class dataCurve:
                     return curveMath.cubicInterp(p_Ky, _Ky, timeIn)
                 elif _Ky.mode == interpMode.LAGRANGE:
                     return curveMath.lagrangeInterp(self.keys, timeIn)
+                elif _Ky.mode == interpMode.QUADRATIC:
+                    return curveMath.quadInterp(p_Ky, _Ky, timeIn)
                 else:
                     return p_Ky.value
         return self.keys[-1].value
--- Tests.py
+++ Tests.py
@@ -1,8 +1,8 @@
-from Main import curveKey, curveMath, dataCurve, interpMode, tangent
+from Main import tang_tension, curveKey, curveMath, dataCurve, interpMode, tangent
 import matplotlib.pyplot as plt
 import random
 
-# RAND_LIST = [1.38, 0.28, 5.96, 5.38, 6.57, 2.51, 6.0, 1.89, 6.61, 5.91, 8.92]
+RAND_LIST = [0.03, 6.6, 1.9, 8.28, 2.44, 0.41, 6.22, 1.43, 2.23, 9.63, 5]
 
 CURVE_DATA = {  # key is distance
     13.0: {'angle': 10.8, 'height': 15},
@@ -10,13 +10,15 @@ CURVE_DATA = {  # key is distance
     27.0: {'angle': 49.9, 'height': 8}
 }
 
+MODE = interpMode.CUBIC
+
 
 def graph(curve: dataCurve, name: str, _min: float, _max: float, _step: float):
     x = []
     y = []
     for i in range(int(_min), int(_max), int(_step)):
-        x.append(i/2)
-        value = curve.eval(i/2)
+        x.append(i)
+        value = curve.eval(i)
         # print(f"{i} : {value}")
         y.append(value)
     plt.plot(x, y, label=name)
@@ -24,31 +26,43 @@ def graph(curve: dataCurve, name: str, _min: float, _max: float, _step: float):
 
 def generateRandomCurve(length: int, step: int) -> list[curveKey]:
     _keys = []
+    x = []
+    y = []
     for i in range(0, length, step):
-        value = random.randint(1, 1000)/100
-        # value = RAND_LIST[int(i/10)]
-        _keys.append(curveKey(i, value, interpMode.CUBIC))
+        # value = random.randint(1, 1000)/100
+        value = RAND_LIST[int(i/100)]
+        _keys.append(curveKey(i, value, MODE))
+        x.append(i)
+        y.append(value)
+    plt.scatter(x, y, label='random scatter', color='red')
     return _keys
 
 
 def generateCurve(field: str, curveDict: dict[float, dict[str, float]]) -> list[curveKey]:
     _keys = []
+
     for i in curveDict:
         value = curveDict[i][field]
-        _keys.append(curveKey(i, value, interpMode.CUBIC))
+        _keys.append(curveKey(i, value, MODE))
+
     return _keys
 
 
 if __name__ == '__main__':
-    ang_curve = dataCurve()
-    ang_curve.addKeys(*generateCurve('angle', CURVE_DATA))
-    ang_curve.finalize()
-    graph(ang_curve, 'Angle', 0, 60, 1)
+    curve = dataCurve()
+    curve.addKeysList(generateRandomCurve(1000, 100))
+    curve.finalize(0)
+    graph(curve, "Random Curve=", 0, 1000, 1)
 
-    hei_curve = dataCurve()
-    hei_curve.addKeys(*generateCurve('height', CURVE_DATA))
-    hei_curve.finalize()
-    graph(hei_curve, 'Height', 0, 60, 1)
+    # ang_curve = dataCurve()
+    # ang_curve.addKeys(*generateCurve('angle', CURVE_DATA))
+    # ang_curve.finalize()
+    # graph(ang_curve, 'Angle', 0, 60, 1)
+
+    # hei_curve = dataCurve()
+    # hei_curve.addKeys(*generateCurve('height', CURVE_DATA))
+    # hei_curve.finalize()
+    # graph(hei_curve, 'Height', 0, 60, 1)
 
     plt.legend()
     plt.show()
